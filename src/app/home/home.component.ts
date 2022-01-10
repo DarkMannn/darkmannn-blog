135217c5c48f34608f5c5aa0437ac57a343d0ed9
DarkMannn <mdarko1993@gmail.com>
2022-01-10 23:25:44 +0100

    feat(.): Fix colors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
     rows = [
-        ['WHO'],
+        ['who'],
         ['developer', 'blogger', 'minimalist'],
-        ['WHAT'],
+        ['what'],
         ['typescript', 'full-stack', 'aws'],
-        ['HOW'],
+        ['how'],
         ['slowly', 'thoroughly', 'with_focus'],
     ];
 
